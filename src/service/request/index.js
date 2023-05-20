6198a432856aca79fa438b79243348f53ddf6659
zilong <2595531421@qq.com>
2023-05-20 22:37:57 +0800

    sixth

--- src/service/request/index.js
+++ src/service/request/index.js
@@ -5,7 +5,8 @@ import useMainStore from "@/stores/modules/main";
 const mainStore = useMainStore();
 
 class myRequest {
-  constructor(baseURL, timeout = 10000) {
+  //构造函数初始化
+  constructor(baseURL, timeout) {
     this.instance = axios.create({
       baseURL,
       timeout,
@@ -13,7 +14,7 @@ class myRequest {
     // 请求拦截器;
     this.instance.interceptors.request.use(
       (config) => {
-        // 在发送请求前显示正在加载
+        //发送请求前-->显示正在加载
         mainStore.isLoading = true;
         return config;
       },
@@ -25,7 +26,7 @@ class myRequest {
     // 响应拦截器;
     this.instance.interceptors.response.use(
       (res) => {
-        // 拿到响应数据后，关闭加载中的显示
+        // 请求成功拿到响应数据后-->关闭加载中的显示
         mainStore.isLoading = false;
         return res;
       },
